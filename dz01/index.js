34990ae2921f5810a51105b1814dac96ed2e0a9d
1Storm3 <zemskoff2013@yandex.ru>
2024-02-28 22:56:23 +0300

    add dz01

---
+++ dz01/index.js
@@ -0,0 +1,31 @@
+import {argv} from 'node:process';
+import process from 'node:process';
+import {stderr, stdout} from 'node:process';
+
+const args = argv.slice(2).map(Number);
+
+if (args.length === 3 && args.every(num => typeof num === 'number')) {
+    let a = args[0];
+    let b = args[1];
+    let c = args[2];
+    console.log('Ваши введённые числа:', a, b, c);
+    const desc = b * b - 4 * a * c;
+    if (desc > 0) {
+        let x1 = (-b - Math.sqrt(desc)) / 2 * a;
+        let x2 = (-b + Math.sqrt(desc)) / 2 * a;
+        console.log('Корень х1 =', x1, 'Корень х2 =', x2)
+        process.exit(0);
+    } else if (desc === 0) {
+        let x1 = -b / 2 * a;
+        let x2 = x1;
+        console.log('Корень х1 =', x1, 'Корень х2 =', x2)
+        process.exit(0);
+    } else {
+        stderr.write('Корней нет,ну или точнее сказать есть комплексные,но это не входит в условие задачи)')
+        process.exit(50);
+    }
+} else if (args[0] || args[1] || args[2] == null) {
+    stdout.write("Вы должны ввести 3 числа, разделяя их пробелом!")
+    process.exit(1);
+}
+
